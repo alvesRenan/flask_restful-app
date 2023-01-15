3f6bb0b990ef4d797c21ca823d50571ee616f3d9
Renan Alves <barbosa.alves.renan@gmail.com>
2023-01-15 15:48:19 -0300

    Update bild script

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -2,6 +2,7 @@
 
 docker build . -t renanalves/test-app
 
-docker push -u renanalves -p $1 renanalves/test-app
+docker login -u renanalves -p "${1}"
+docker push renanalves/test-app
 
 kubectl apply -f k8s-files
